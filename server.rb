7932d85cd5602e78d5e79055e777f9bb7c088d95
Takuma Nakajima <penguin.trance.2716@gmail.com>
2013-07-30 23:13:51 +0900

    死んでしまうところだった

--- server.rb
+++ server.rb
@@ -6,10 +6,10 @@ require 'twitter'
 SOURCE = File.expand_path(File.join(File.dirname(__FILE__), 'source.rb'))
 
 ## Twitterのキーがある人は設定すると幸せになれる
-# TWITTER_CONSUMER_KEY    = "AmDS1hCCXWstbss5624kVw"
-# TWITTER_CONSUMER_SECRET = "KOPOooopg9Scu7gJUBHBWjwkXz9xgPJxnhnhO55VQ"
-# TWITTER_TOKEN           = "66159300-uigYA04OU4O1PhFCBTHXRJJ1F1hEXSAICSps6Mz9k"
-# TWITTER_SECRET          = "80kW7WMFGzZ2t6n0uBJtEidPN3igpAhIYd9XJorSE"
+# TWITTER_CONSUMER_KEY    = "AmDS1hCCXW"
+# TWITTER_CONSUMER_SECRET = "KOPOooopg9"
+# TWITTER_TOKEN           = "66159300-u"
+# TWITTER_SECRET          = "80kW7WMFGz"
 # Twitter.configure do |c|
 #   c.consumer_key       = TWITTER_CONSUMER_KEY
 #   c.consumer_secret    = TWITTER_CONSUMER_SECRET
